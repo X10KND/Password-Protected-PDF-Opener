06026f6ce536d895b8fe715eb65ccecc04235d20
Asef Jamil Ajwad <sigma.asef@gmail.com>
2023-03-03 02:36:43 +0600

    Update PDF Opener.py

--- PDF Opener.py
+++ PDF Opener.py
@@ -16,7 +16,7 @@ capital_letters = False
 numbers = True
 spaces = False
 
-def combomaker(options, depth, prefix, suffix, past = "", arr = []):
+def combomaker(options, depth, prefix, suffix, past, arr):
     for i in options:
         if depth > 1:
             combomaker(options, depth - 1, prefix, suffix, past + str(i), arr)
@@ -57,7 +57,7 @@ if __name__ == '__main__':
     if spaces:
         options += " "
         
-    passwords = combomaker(options, length, PREFIX, SUFFIX)
+    passwords = combomaker(options, length, PREFIX, SUFFIX, "", [])
     print(f"{len(passwords)} combinations")
 
     if os.path.isfile(filename):
